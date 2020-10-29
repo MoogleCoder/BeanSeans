8e46dce918bb38a4e6204fa22353e166cfa903e8
Frank Yu <frank.yu@tafestudynsw.edu.au>
2020-10-29 13:55:57 +1100

    Add project files.

---
+++ BeanSeans/Models/TableReservation/CreateTableReservation.cs
@@ -0,0 +1,15 @@
+﻿using BeanSeans.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanSeans.Models.TableReservation
+{
+    public class CreateTableReservation
+    {
+        public Table  Table { get; set; }
+
+        public Data.Reservation Reservation { get; set; }
+    }
+}
